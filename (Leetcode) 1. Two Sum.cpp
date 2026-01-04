2782038cc9d48d0ea118839a84e05df0a4552874
Aryan Das <aryandas2911@gmail.com>
2026-01-05 01:18:03 +0530

    (Leetcode) 9. Palindrome Number (solved)

--- (Leetcode) 1. Two Sum.cpp
+++ (Leetcode) 1. Two Sum.cpp
@@ -23,3 +23,15 @@ public:
         return {};
     }
 };
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = s.twoSum(nums, target);
+
+    cout << result[0] << " " << result[1] << endl;
+    return 0;
+}
---
+++ (Leetcode) 9. Palindrome Number.cpp
@@ -0,0 +1,41 @@
+// Ques Link: https://leetcode.com/problems/palindrome-number/description/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x==0) return true;
+        int a=x;
+        int s=0;
+        if (a>0){
+            while (a!=0){
+                int r=a%10;
+                if (s>INT_MAX/10) return false;
+                s=s*10+r;
+                a=a/10;
+            }
+            if (s==x){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int x;
+    cin >> x;
+    cout << sol.isPalindrome(x);
+
+    return 0;
+}
