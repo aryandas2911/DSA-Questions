8895f38f0d9b55e053a176be20da80d20caf6f67
Aryan Das <aryandas2911@gmail.com>
2026-01-07 22:07:18 +0530

    (Leetcode) 1486. XOR Operation in an Array (solved)

---
+++ (Leetcode) 1486. XOR Operation in an Array.cpp
@@ -0,0 +1,29 @@
+// Ques Link: https://leetcode.com/problems/xor-operation-in-an-array/description/
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int nums[n];
+        int x=0;
+        for (int i=0;i<n;i++){
+            nums[i]=start+2*i;
+        }
+        for (auto i:nums){
+            x^=i;
+        }
+        return x;
+    }
+};
+
+int main()
+{
+    Solution s;
+    int n, start;
+    cin >> n >> start;
+    int k = s.xorOperation(n, start);
+    cout << k;
+    return 0;
+}
