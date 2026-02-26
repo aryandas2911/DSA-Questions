47e8eabd0feb5cd73860f5514efbaa7b30dc15cc
Aryan Das <aryandas2911@gmail.com>
2026-02-26 21:51:02 +0530

    (Leetcode) 169. Majority Element (Optimized)

--- Sorting/Easy/(Leetcode) 169. Majority Element.cpp
+++ Sorting/Easy/(Leetcode) 169. Majority Element.cpp
@@ -11,15 +11,44 @@ class Solution
 public:
     int majorityElement(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        int ele;
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (cnt == 0)
+            {
+                ele = nums[i];
+                cnt = 1;
+            }
+            else if (nums[i] == ele)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == ele)
+            {
+                count++;
+            }
+        }
+        if (count > (nums.size() / 2))
+        {
+            return ele;
+        }
+        return -1;
     }
 };
 
 int main()
 {
     Solution sol;
-    vector<int> nums1 = {3, 2, 3};
+    vector<int> nums1 = {2,2,1,1,1,2,2};
     cout << sol.majorityElement(nums1) << endl;
     return 0;
 }
