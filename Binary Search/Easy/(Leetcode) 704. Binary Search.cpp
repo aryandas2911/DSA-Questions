90381c078db87d7b862942128b4e2819d799b5b4
Aryan Das <aryandas2911@gmail.com>
2026-06-26 00:02:59 +0530

    (Leetcode) 704. Binary Search (solved)

---
+++ Binary Search/Easy/(Leetcode) 704. Binary Search.cpp
@@ -0,0 +1,50 @@
+// Ques Link: https://leetcode.com/problems/binary-search/description/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            if (target < nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+
+    Solution sol;
+    int result = sol.search(nums, target);
+
+    cout << "Index: " << result << endl;
+
+    return 0;
+}
