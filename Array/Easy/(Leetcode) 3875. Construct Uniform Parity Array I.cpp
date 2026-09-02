3d0da30f8f71cda34d4ba4e51b4566ad9f352a27
Aryan Das <aryandas2911@gmail.com>
2026-09-02 21:29:52 +0530

    (Leetcode) 3875. Construct Uniform Parity Array I

---
+++ Array/Easy/(Leetcode) 3875. Construct Uniform Parity Array I.cpp
@@ -0,0 +1,24 @@
+// Ques Link: https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool uniformArray(vector<int> &nums1)
+    {
+        return true;
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    vector<int> nums1 = {1, 2, 3, 4};
+
+    cout << boolalpha << obj.uniformArray(nums1) << endl;
+
+    return 0;
+}
