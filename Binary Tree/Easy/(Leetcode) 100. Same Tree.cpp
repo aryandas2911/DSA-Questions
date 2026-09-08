f1fc86f662812957e8ff102a76afe9aeea5761fe
Aryan Das <aryandas2911@gmail.com>
2026-09-08 21:18:29 +0530

    (Leetcode) 100. Same Tree

---
+++ Binary Tree/Easy/(Leetcode) 100. Same Tree.cpp
@@ -0,0 +1,60 @@
+// Ques Link: https://leetcode.com/problems/same-tree/description/
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (p == NULL || q == NULL)
+        {
+            return (p == q);
+        }
+
+        return (p->val == q->val) &&
+               isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+};
+
+int main()
+{
+    // Tree 1:    1
+    //           / \
+    //          2   3
+
+    TreeNode *p = new TreeNode(1);
+    p->left = new TreeNode(2);
+    p->right = new TreeNode(3);
+
+    // Tree 2:    1
+    //           / \
+    //          2   3
+
+    TreeNode *q = new TreeNode(1);
+    q->left = new TreeNode(2);
+    q->right = new TreeNode(3);
+
+    Solution obj;
+
+    if (obj.isSameTree(p, q))
+        cout << "Trees are identical" << endl;
+    else
+        cout << "Trees are not identical" << endl;
+
+    return 0;
+}
